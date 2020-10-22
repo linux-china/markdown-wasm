74e5157d6236355693f5193928561814afc1e1c7
linux_china <libing.chen@gmail.com>
2020-10-22 13:20:36 -0700

    polishing

--- demo.ts
+++ demo.ts
@@ -1,3 +1,4 @@
-import {parseMarkdown} from "./mod.ts"
+import {parseMarkdown} from "https://denopkg.com/linux-china/markdown-wasm/mod.ts"
+// import {parseMarkdown} from "./mod.ts"
 
 console.log(parseMarkdown("# hello\n*世界*"))
